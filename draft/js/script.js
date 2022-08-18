1b93000f69205639fe1a0c3ce522989d4faf07c9
kashkadv <kashkadmuitry1991@gmail.com>
2022-08-18 13:17:40 +0300

    Lesson 24

--- draft/js/script.js
+++ draft/js/script.js
@@ -1,13 +1,33 @@
 const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели', ''),
       personalMovieDB = {
-        count: numberOfFilms,
+        count: +numberOfFilms,
         movies: {},
         actors: {},
         genres: [],
         privat: false
       }
 
-personalMovieDB.movies[prompt('Один из последних просмотренных фильмов?', '')] = prompt('На сколько оцените его?', '');
-personalMovieDB.movies[prompt('Один из последних просмотренных фильмов?', '')] = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+    let movieTitle = '';
+    let movieScore = '';
+
+    while(movieScore === null || movieScore.length < 1 || movieTitle === null || movieTitle.length < 1 || movieTitle.length > 50) {
+        movieTitle = prompt('Один из последних просмотренных фильмов?', '');
+        movieScore = prompt('На сколько оцените его?', '');
+    }
+
+    personalMovieDB.movies[movieTitle] = movieScore;
+}
+
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
+
